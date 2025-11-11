dd1f6d89fdb32678d0c953c66badd08bc96f544c
Fernando Castro <fernand4603b@gmail.com>
2025-11-11 21:32:31 +0000

    tentando usar as novas ferramentas na tarefa do pula pula

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -1,34 +1,68 @@
 class Kid:
-    def __init__(self, nome:str, idade:int):
-        self.__nome = nome
-        self.__idade = idade
-    
-    def getIdd(self)->str:
+    def __init__(self,nome:str, idade:int) -> None:
+        if nome== "":
+            raise Exception("fail: nome não pode ser vazio")
+            
+        elif idade <= 0:
+            raise Exception("fail: idade inválida")
+            
+
+        self.__nome =nome 
+        self.__idade=idade
+
+    def getNome(self):
         return self.__nome
-
-    def getNome (self)->int:
-        return self.__idade
-
-    def setNome(self, nome:str):
-        self.__nome = nome
     
-    def __str__(self) -> str:
-        return f"{self.__nome} e {self.__idade}"
-
+    def getIdade(self):
+        return self.__idade 
+    
+    def setNome(self, nome:str):
+        if nome.strip() == "":
+            raise Exception ( " fail: nome vazio")
+        self.__nome = nome
+    def setIdade(self, idade:int):
+        if idade <= 0:
+            raise Exception(" fail: idade inválida")
+        self.__idade =idade
+    
 class Pula_pula:
-    def __init__(self, num_brincando:int) -> None:
-        self.brincando: list[Kid | None] = [None for _ in range(num_brincando)]
+    def __init__(self) -> None:
         self.espera: list[Kid] = []
+        self.pular: list[Kid] = []
 
     def entrar(self, kid: Kid):
         self.espera.append(kid)
-
-    def chamar(self, index:int):
-        if index < 0 or index >= len(self.brincando):
-            print("fail: ninguém brincando")
-            return
-        
-        if self.brincando[index] is not None:
-            print("")
-
     
+    def chamar(self):
+        try:
+            if len(self.espera) <= 0:
+                raise Exception ("fail: fila vazia")
+            self.pular.append(self.espera.pop(0))
+        except Exception as e:
+            print(e)
+            
+    def sair(self ):
+        try:
+            if len(self.pular) <= 0:
+                raise Exception ("fail: ninguem brincanado")
+            self.espera.append(self.pular.pop(0))
+        except Exception as e:
+            print(e)
+                
+            #testando a sintaxe de cima
+            #self.espera.append(self.pular.pop(0))
+            #try:
+            #    if len(self.pular) <=0:
+            #    raise Exception ("fail: ninguem brincando")
+            #except Exception as e:
+            #    print(e
+            
+            
+
+
+            
+
+
+        
+
+
