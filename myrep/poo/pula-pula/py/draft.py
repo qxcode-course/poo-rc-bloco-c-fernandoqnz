4d932539b21708af14804435339f8de4cb582a0e
Fernando Castro <fernand4603b@gmail.com>
2025-11-11 13:28:48 +0000

    refazendo

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -1,54 +1,34 @@
 class Kid:
-    def __init__(self, nome: str, idd: int) -> None:
-        self.nome = nome
-        self.idd = idd
+    def __init__(self, nome:str, idade:int):
+        self.__nome = nome
+        self.__idade = idade
+    
+    def getIdd(self)->str:
+        return self.__nome
 
+    def getNome (self)->int:
+        return self.__idade
+
+    def setNome(self, nome:str):
+        self.__nome = nome
+    
     def __str__(self) -> str:
-        return f"{self.nome}:{self.idd}"
-
+        return f"{self.__nome} e {self.__idade}"
 
 class Pula_pula:
-    def __init__(self, capacidade: int) -> None:
-        self.capacidade = capacidade
-        self.brincando: list[Kid | None] = [None for _ in range(capacidade)]
+    def __init__(self, num_brincando:int) -> None:
+        self.brincando: list[Kid | None] = [None for _ in range(num_brincando)]
         self.espera: list[Kid] = []
 
     def entrar(self, kid: Kid):
         self.espera.append(kid)
 
-    def chamar(self, index: int):
+    def chamar(self, index:int):
         if index < 0 or index >= len(self.brincando):
-            print("fail: índice inexistente")
+            print("fail: ninguém brincando")
             return
+        
         if self.brincando[index] is not None:
-            print("fail: ocupado")
-            return
-        if len(self.espera) == 0:
-            print("fail: sem crianças esperando")
-            return
-        self.brincando[index] = self.espera.pop(0)
-
-    def finalizar(self, index: int):
-        if index < 0 or index >= len(self.brincando):
-            print("fail: índice inexistente")
-            return
-        if self.brincando[index] is None:
-            print("fail: vazio")
-            return
-        self.brincando[index] = None
-
-    def sair(self, nome: str):
-        for i, kid in enumerate(self.espera):
-            if kid.nome == nome:
-                del self.espera[i]
-                return
-        print("fail: criança não encontrada")
-
-    def __str__(self):
-        brincando = ", ".join([str(kid) if kid else "-" for kid in self.brincando])
-        esperando = ", ".join([str(kid) for kid in self.espera])
-        return f"[ {brincando} ] => Espera: [ {esperando} ]"
-
+            print("")
 
     
-        
